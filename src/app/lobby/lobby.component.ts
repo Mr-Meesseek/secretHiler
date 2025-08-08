070fc245e544f51ae2c555b56d576729848232e4
Mr-Meesseek <ismailsalemliamsi@gmail.com>
2025-08-07 17:43:50 -1100

    first lobby page

---
+++ src/app/lobby/lobby.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-lobby',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './lobby.component.html',
+  styleUrls: ['./lobby.component.scss']
+})
+export class LobbyComponent {
+
+  gameDate: string = '';
+  teamMode: number = 2;
+
+  startGame() {
+    console.log('Game started with:', {
+      
+      
+      teamMode: this.teamMode,
+    });
+  }
+}
